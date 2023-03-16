901f0471cc5cd695990dd94d7ba609926f9d1d77
Elias Evertz <Elias.Evertz@NMG-Group.com>
2023-03-16 16:20:39 -0400

    moved some extract functionalities from main to extract

--- etl/extract.py
+++ etl/extract.py
@@ -23,14 +23,29 @@ class Extract():
         return res_token
 
     def get_api_response(self, pSurvey):
-        token = self.get_api_token()
-        # token['access_token']
+        token = self.get_api_token()        
         vAPI_ENDPOINT_RESPONSES = myAPI_ENDPOINT_RESPONSES.replace("[param_survey]", pSurvey)
         vAPI_API_HEATHERS = json.loads(myAPI_API_HEATHERS)
-        vAPI_API_HEATHERS['Authorization'] = "Bearer "+token['access_token']
-        # res = vAPI_API_HEATHERS
+        vAPI_API_HEATHERS['Authorization'] = "Bearer "+token['access_token']        
         res = u.api_call_request('GET', vAPI_ENDPOINT_RESPONSES, vAPI_API_HEATHERS, '')
         return res
+    
+    def read_api_json(self, pSurvey):
+        res = self.get_api_response(pSurvey)
+        df_survey_flat = pd.json_normalize(res, record_path="items")
+        df_survey_flat.columns = df_survey_flat.columns.str.replace(".", "_", regex=False)
+        df_survey_flat.columns = df_survey_flat.columns.str.replace("$", "_", regex=False)
+        # df_survey_flat.columns = df_survey_flat.columns.str.replace('"', '_', regex=False)
+
+        #converting array values to string in every column
+        all_columns = list(df_survey_flat) #to get column headers
+        for i in all_columns:
+            val = str(df_survey_flat[i][0])
+            if "[" in val:
+                df_survey_flat[i] = [','.join(map(str, l)) for l in df_survey_flat[i]]
+        return df_survey_flat
+    
+    ######## to Read from files in determine path ########
 
     def read_csv(self, path_file, delimiter, name_headers, headers=None):
        
